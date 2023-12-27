4b182fde9311946595c2f22195b9e23646a59dd4
Silverlan <silverlann@gmail.com>
2023-12-27 12:13:39 +0100

    feat: add "Body" property for vr_body component

--- lua/entities/components/vr_body/client/cl_init.lua
+++ lua/entities/components/vr_body/client/cl_init.lua
@@ -6,15 +6,20 @@
     file, You can obtain one at http://mozilla.org/MPL/2.0/.
 ]]
 
-util.register_class("ents.VrBody", BaseEntityComponent)
+local Component = util.register_class("ents.VrBody", BaseEntityComponent)
 
-ents.VrBody:RegisterMember("Enabled", udm.TYPE_BOOLEAN, true, {
+Component:RegisterMember("Enabled", udm.TYPE_BOOLEAN, true, {
 	onChange = function(self)
 		self:UpdateActiveState()
 	end,
 }, bit.bor(ents.BaseEntityComponent.MEMBER_FLAG_DEFAULT, ents.BaseEntityComponent.MEMBER_FLAG_BIT_USE_IS_GETTER))
+Component:RegisterMember("Body", ents.MEMBER_TYPE_ELEMENT, "", {
+	onChange = function(self)
+		self:UpdateBody()
+	end,
+}, bit.bor(ents.BaseEntityComponent.MEMBER_FLAG_DEFAULT))
 
-function ents.VrBody:Initialize()
+function Component:Initialize()
 	BaseEntityComponent.Initialize(self)
 
 	local vrIkC = self:AddEntityComponent("vr_ik")
@@ -26,21 +31,95 @@ function ents.VrBody:Initialize()
 	end)
 end
 
-function ents.VrBody:OnRemove()
+function Component:UpdateBody()
+	local udmVrBody = self:GetBody()
+	local headBone = udmVrBody:GetValue("headBone", udm.TYPE_STRING) or ""
+	local upperBodyBoneChain = udmVrBody:GetArrayValues("upperBodyBoneChain", udm.TYPE_STRING)
+	local leftArmBoneChain = udmVrBody:GetArrayValues("leftArmBoneChain", udm.TYPE_STRING)
+	local rightArmBoneChain = udmVrBody:GetArrayValues("rightArmBoneChain", udm.TYPE_STRING)
+
+	--[[local retargetC = entImpostor:GetComponent("retarget_rig")
+	if retargetC ~= nil then
+		pfm.log("Found retarget rig, applying vr_body bone translations...", pfm.LOG_CATEGORY_VRP)
+		-- Translate bones
+		for i, boneName in ipairs(upperBodyBoneChain) do
+			upperBodyBoneChain[i] = retargetC:TranslateBoneFromTarget(mdl:LookupBone(boneName))[1] or -1
+			if upperBodyBoneChain[i] == -1 then
+				pfm.log(
+					"Could not map bone '"
+						.. boneName
+						.. "' for upper body ik of impostor, because no mapping could be found!",
+					pfm.LOG_CATEGORY_VRP,
+					pfm.LOG_SEVERITY_WARNING
+				)
+			end
+		end
+		for i, boneName in ipairs(leftArmBoneChain) do
+			leftArmBoneChain[i] = retargetC:TranslateBoneFromTarget(mdl:LookupBone(boneName))[1] or -1
+			if leftArmBoneChain[i] == -1 then
+				pfm.log(
+					"Could not map bone '"
+						.. boneName
+						.. "' for left arm ik of impostor, because no mapping could be found!",
+					pfm.LOG_CATEGORY_VRP,
+					pfm.LOG_SEVERITY_WARNING
+				)
+			end
+		end
+		for i, boneName in ipairs(rightArmBoneChain) do
+			rightArmBoneChain[i] = retargetC:TranslateBoneFromTarget(mdl:LookupBone(boneName))[1] or -1
+			if rightArmBoneChain[i] == -1 then
+				pfm.log(
+					"Could not map bone '"
+						.. boneName
+						.. "' for right arm ik of impostor, because no mapping could be found!",
+					pfm.LOG_CATEGORY_VRP,
+					pfm.LOG_SEVERITY_WARNING
+				)
+			end
+		end
+		-- Remove ik nodes if no mapping exists for them; The ik may work anyway
+		for _, ikChain in ipairs({ upperBodyBoneChain, leftArmBoneChain, rightArmBoneChain }) do
+			for i = #ikChain, 1, -1 do
+				if ikChain[i] == -1 then
+					table.remove(ikChain, i)
+				end
+			end
+		end
+		headBone = retargetC:TranslateBoneFromTarget(mdl:LookupBone(headBone))[1] or -1
+		pfm.log("Retargeted head bone for '" .. headBone .. "': " .. tostring(headBone), pfm.LOG_CATEGORY_VRP)
+	end]]
+
+	if #upperBodyBoneChain > 2 then
+		self:SetUpperBody(upperBodyBoneChain)
+	end
+	if #leftArmBoneChain > 2 then
+		self:SetLeftArm(leftArmBoneChain)
+	end
+	if #rightArmBoneChain > 2 then
+		self:SetRightArm(rightArmBoneChain)
+	end
+	if headBone ~= -1 then
+		self:SetHeadBone(headBone)
+	end
+end
+
+function Component:OnRemove()
 	util.remove(self.m_povCamOnAvailabilityChangedCb)
 	util.remove(self.m_cbPreIkTreesUpdates)
 	util.remove(self.m_cbOnTrackedDeviceAdded)
 	util.remove(self.m_cbOnTrackedDeviceActivated)
 end
 
-function ents.VrBody:OnEntitySpawn()
+function Component:OnEntitySpawn()
+	self:UpdateBody()
 	--[[local mdlC = self:GetEntity():GetComponent(ents.COMPONENT_PFM_MODEL)
 	if(mdlC == nil) then return end
 	mdlC:SetAnimationFrozen(true)]]
 	-- TODO: Remove this once the issue with ik on animated entities is fixed
 end
 
-function ents.VrBody:ResetIk()
+function Component:ResetIk()
 	local vrIkC = self:GetEntity():GetComponent(ents.COMPONENT_VR_IK)
 	if vrIkC == nil then
 		return
@@ -50,7 +129,7 @@ function ents.VrBody:ResetIk()
 	vrIkC:ResetIkTree("right_arm")
 end
 
-function ents.VrBody:AdjustUpperBody()
+function Component:AdjustUpperBody()
 	local ent = self:GetEntity()
 	local vrIkC = ent:GetComponent(ents.COMPONENT_VR_IK)
 	if vrIkC == nil or util.is_valid(self.m_povCamera) == false then
@@ -112,7 +191,7 @@ function ents.VrBody:AdjustUpperBody()
 	end]]
 end
 
-function ents.VrBody:UpdateRelativeIkUpperBodyPose()
+function Component:UpdateRelativeIkUpperBodyPose()
 	-- pov_camera can have a relative pose that is added as an offset to the head bone, however
 	-- we don't want that offset to affect our upper_body ik tree (because it would defeat the purpose of the offset).
 	-- For this reason, we'll give the upper_body ik tree the inverse offset, effectively placing it at the exact location of the head bone.
@@ -123,13 +202,13 @@ function ents.VrBody:UpdateRelativeIkUpperBodyPose()
 	vrIk:SetIkControllerPoseOffset("upper_body",relPose:GetInverse())]]
 end
 
-function ents.VrBody:UpdateActiveState()
+function Component:UpdateActiveState()
 	if self:IsEnabled() then
 	else
 	end
 end
 
-function ents.VrBody:HideHeadBone(hide)
+function Component:HideHeadBone(hide)
 	if hide == nil then
 		hide = true
 	end
@@ -144,7 +223,7 @@ function ents.VrBody:HideHeadBone(hide)
 	--self:AdjustUpperBody()
 end
 
-function ents.VrBody:SetPovCamera(cam)
+function Component:SetPovCamera(cam)
 	util.remove(self.m_povCamOnAvailabilityChangedCb)
 	self.m_povCamera = cam
 	local vrIkC = self:GetEntity():GetComponent(ents.COMPONENT_VR_IK)
@@ -170,7 +249,7 @@ function ents.VrBody:SetPovCamera(cam)
 	self:UpdatePovCameraAvailability()
 end
 
-function ents.VrBody:SetHmd(hmd)
+function Component:SetHmd(hmd)
 	util.remove(self.m_cbOnTrackedDeviceAdded)
 	util.remove(self.m_cbOnTrackedDeviceActivated)
 	self.m_hmdC = hmd
@@ -196,11 +275,11 @@ function ents.VrBody:SetHmd(hmd)
 	self:UpdateTrackedDeviceVisibility()
 	self:UpdatePovCameraAvailability()
 end
-function ents.VrBody:GetHmd()
+function Component:GetHmd()
 	return self.m_hmdC
 end
 
-function ents.VrBody:UpdatePovCameraAvailability()
+function Component:UpdatePovCameraAvailability()
 	if util.is_valid(self.m_povCamera) == false then
 		return
 	end
@@ -214,7 +293,7 @@ function ents.VrBody:UpdatePovCameraAvailability()
 	self.m_povCamera:SetEnabled(enablePovCamera)
 end
 
-function ents.VrBody:UpdateTrackedDeviceVisibility()
+function Component:UpdateTrackedDeviceVisibility()
 	local vrIk = self:GetEntity():GetComponent(ents.COMPONENT_VR_IK)
 	if util.is_valid(self.m_hmdC) == false or vrIk == nil then
 		return
@@ -236,7 +315,7 @@ function ents.VrBody:UpdateTrackedDeviceVisibility()
 	end
 end
 
-function ents.VrBody:UpdateTrackedDevices()
+function Component:UpdateTrackedDevices()
 	local vrIk = self:GetEntity():GetComponent(ents.COMPONENT_VR_IK)
 	if util.is_valid(self.m_hmdC) == false or vrIk == nil then
 		return
@@ -276,7 +355,7 @@ function ents.VrBody:UpdateTrackedDevices()
 	end
 end
 
-function ents.VrBody:SetLeftArm(boneChain)
+function Component:SetLeftArm(boneChain)
 	local vrIk = self:GetEntity():GetComponent(ents.COMPONENT_VR_IK)
 	if vrIk == nil then
 		return
@@ -287,7 +366,7 @@ function ents.VrBody:SetLeftArm(boneChain)
 	vrIk:SetIkControllerEnabled("left_arm", false)
 end
 
-function ents.VrBody:SetRightArm(boneChain)
+function Component:SetRightArm(boneChain)
 	local vrIk = self:GetEntity():GetComponent(ents.COMPONENT_VR_IK)
 	if vrIk == nil then
 		return
@@ -298,7 +377,7 @@ function ents.VrBody:SetRightArm(boneChain)
 	vrIk:SetIkControllerEnabled("right_arm", false)
 end
 
-function ents.VrBody:SetHeadBone(boneId)
+function Component:SetHeadBone(boneId)
 	self.m_headBone = nil
 	if type(boneId) == "string" then
 		local mdl = self:GetEntity():GetModel()
@@ -310,11 +389,11 @@ function ents.VrBody:SetHeadBone(boneId)
 	self.m_headBone = (boneId ~= -1) and boneId or nil
 end
 
-function ents.VrBody:GetHeadBoneId()
+function Component:GetHeadBoneId()
 	return self.m_headBone
 end
 
-function ents.VrBody:SetUpperBody(boneChain)
+function Component:SetUpperBody(boneChain)
 	local vrIk = self:GetEntity():GetComponent(ents.COMPONENT_VR_IK)
 	if vrIk == nil then
 		return
@@ -324,4 +403,4 @@ function ents.VrBody:SetUpperBody(boneChain)
 	vrIk:SetEffectorPos("upper_body", Vector(0, 0, 0))
 	vrIk:SetIkControllerEnabled("upper_body", false)
 end
-ents.COMPONENT_VR_BODY = ents.register_component("vr_body", ents.VrBody)
+ents.COMPONENT_VR_BODY = ents.register_component("vr_body", Component)
